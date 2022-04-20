330a7ea85f83c617d833e8bfb262caf233ff2097
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-20 22:30:22 +0800

    Use default

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,15 @@ import * as windows from "./windows";
 async function run() {
     try {
         switch (process.platform) {
-            case "linux":
-            case "darwin": {
+            case "win32": {
+                await windows.run();
+            } break;
+            default: {
                 const version = core.getInput("version");
                 const emacsCIVersion = "emacs-" + version.replace(".", "-");
                 await installNix.run(emacsCIVersion);
             } break;
-            case "win32": {
-                await windows.run();
-            } break;
         }
-
     } catch (error) {
         let errorMsg = "Failed to do something exceptional";
         if (error instanceof Error) {
