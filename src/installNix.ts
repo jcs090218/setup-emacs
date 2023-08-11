7e6ba9cb747cb13448206edbe79dcfb4bdb8c272
Paul Pogonyshev <pogonyshev@gmail.com>
2023-08-11 21:25:10 +0200

    Simplify the action by making it 'composite' instead of Node.js-based

--- src/installNix.ts
+++
@@ -1,8 +0,0 @@
-// Based on https://github.com/cachix/install-nix-action
-import { execFileSync } from 'child_process';
-import fs from 'fs';
-
-export function run(emacsCIVersion : string) {
-    fs.chmodSync(`${__dirname}/install-nix.sh`, 0o755);
-    execFileSync(`${__dirname}/install-nix.sh`, [emacsCIVersion], { stdio: 'inherit' });
-}
